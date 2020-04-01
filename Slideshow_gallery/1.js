a2c50914885c3e03c677a9be8a87c6d7fd16c866
Dog99 <quangsoi99@gmail.com>
2020-04-01 17:50:35 +0700

    Slideshow-Gallery

---
+++ Slideshow_gallery/1.js
@@ -0,0 +1,31 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var slides = document.getElementsByClassName("mySlide");
+	var dots = document.getElementsByClassName("demo");
+	var caption = document.getElementById("caption");
+
+	if (n > slides.length) {slideIndex = 1;};
+	if (n < 1) {slideIndex = slides.length}
+
+	for (var i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";
+			dots[i].className = dots[i].className.replace(" active", "");
+	}
+
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+
+	console.log(dots[slideIndex-1]);
+	caption.innerHTML = dots[slideIndex - 1].alt;
+}
+
